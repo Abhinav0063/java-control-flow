343423e5f6870ca70ae7d7322e03e925d06cb4f3
Abhinav's MacBook Air M3 <psiecsaalry@gmail.com>
2025-03-04 10:07:38 +0530

    [Abhinav] ADD: MultiplesBelow100.java

---
+++ MultiplesBelow100.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MultiplesBelow100 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a positive number less than 100: ");
+        int number = input.nextInt();
+
+        if (number <= 0 || number >= 100) {
+            System.out.println("Please enter a number between 1 and 99.");
+        } else {
+            System.out.print("Multiples of " + number + " below 100: ");
+            for (int i = 100; i >= 1; i--) {
+                if (i % number == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+        input.close();
+    }
+}
