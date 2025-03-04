a9b38889bbb759894d11e02f0e56a4628ef3b761
Abhinav's MacBook Air M3 <psiecsaalry@gmail.com>
2025-03-04 10:04:37 +0530

    [Abhinav] ADD: FactorsWhileLoop.java

---
+++ FactorsWhileLoop.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FactorsWhileLoop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a positive number: ");
+        int number = input.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            int i = 1;
+            System.out.print("Factors of " + number + " are: ");
+            while (i <= number) {
+                if (number % i == 0) {
+                    System.out.print(i + " ");
+                }
+                i++;
+            }
+        }
+        input.close();
+    }
+}
