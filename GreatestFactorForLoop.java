d62cdba3b00a52e981c58e88eadd259fea0d7636
Abhinav's MacBook Air M3 <psiecsaalry@gmail.com>
2025-03-04 10:05:59 +0530

    [Abhinav] ADD: GreatestFactorForLoop.java

---
+++ GreatestFactorForLoop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GreatestFactorForLoop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a positive number: ");
+        int number = input.nextInt();
+
+        int greatestFactor = 1;
+        for (int i = number - 1; i >= 1; i--) {
+            if (number % i == 0) {
+                greatestFactor = i;
+                break;
+            }
+        }
+
+        System.out.println("Greatest factor beside itself is: " + greatestFactor);
+        input.close();
+    }
+}
